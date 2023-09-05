17c5d8100d226a2cd8e2f35293f2f2a46609bfff
GregCurtin <141890207+GregCurtin@users.noreply.github.com>
2023-09-05 11:19:06 -0400

    Add files via upload

---
+++ run_wigner.sh
@@ -0,0 +1,2 @@
+ml Anaconda2 #adjust this line to get python 2 
+python /global/user_software/wigner/0.1/wigner.py --temp 300 --hessian Hessian.bin --wigner --wigner-dir wigner --wigner-N 100 --viz --viz-dir viz --viz-dx 20.0 > wigner.log
